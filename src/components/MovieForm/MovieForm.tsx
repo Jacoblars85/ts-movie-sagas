393f184236a1ab49b17c866023663d99baf7d787
Jacob Larson <hockeybobber@comcast.net>
2024-11-28 14:30:07 -0600

    added inputs

--- src/components/MovieForm/MovieForm.tsx
+++ src/components/MovieForm/MovieForm.tsx
@@ -4,8 +4,26 @@ function MovieForm() {
 
   return (
     <div>
+    <h2>Add a Movie</h2>
 
-    </div>
+    <input type="text"
+        placeholder="Enter Title"
+        onChange={(e) => setTitleInput(e.target.value)}
+        value={titleInput} />
+
+    <input type="text"
+        placeholder="Enter Description"
+        onChange={(e) => setDescriptionInput(e.target.value)}
+        value={descriptionInput} />
+
+    <input type="text"
+        placeholder="Enter URL"
+        onChange={(e) => setUrlInput(e.target.value)}
+        value={urlInput} />
+
+    <button onClick={addPassenger}>Add Passenger</button>
+
+</div>
   );
 }
 
