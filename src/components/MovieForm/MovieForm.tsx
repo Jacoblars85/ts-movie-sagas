556a242f285596e200aacd56a0e56b4fca18be36
Jacob Larson <hockeybobber@comcast.net>
2024-11-29 23:28:07 -0600

    formatted doc

--- src/components/MovieForm/MovieForm.tsx
+++ src/components/MovieForm/MovieForm.tsx
@@ -1,51 +1,54 @@
-import { useSelector, useDispatch } from 'react-redux'
-import React, { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
 
 function MovieForm() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const [titleInput, setTitleInput] = useState("");
+  const [descriptionInput, setDescriptionInput] = useState("");
+  const [urlInput, setUrlInput] = useState("");
 
-    const [titleInput, setTitleInput] = useState('');
-    const [descriptionInput, setDescriptionInput] = useState('');
-    const [urlInput, setUrlInput] = useState('');
-
-    const addPassenger = () => {
-        dispatch({
-            type: 'SAGA_POST_MOVIE',
-            payload: {
-                title: titleInput,
-                poster: urlInput,
-                description: descriptionInput
-
-            }
-        })
-        setTitleInput('')
-        setDescriptionInput('')
-        setUrlInput('')
-    }
+  const addPassenger = () => {
+    dispatch({
+      type: "SAGA_POST_MOVIE",
+      payload: {
+        title: titleInput,
+        poster: urlInput,
+        description: descriptionInput,
+      },
+    });
+    setTitleInput("");
+    setDescriptionInput("");
+    setUrlInput("");
+  };
 
   return (
     <div>
-    <h2>Add a Movie</h2>
+      <h2>Add a Movie</h2>
 
-    <input type="text"
+      <input
+        type="text"
         placeholder="Enter Title"
         onChange={(e) => setTitleInput(e.target.value)}
-        value={titleInput} />
+        value={titleInput}
+      />
 
-    <input type="text"
+      <input
+        type="text"
         placeholder="Enter Description"
         onChange={(e) => setDescriptionInput(e.target.value)}
-        value={descriptionInput} />
+        value={descriptionInput}
+      />
 
-    <input type="text"
+      <input
+        type="text"
         placeholder="Enter URL"
         onChange={(e) => setUrlInput(e.target.value)}
-        value={urlInput} />
+        value={urlInput}
+      />
 
-    <button onClick={addPassenger}>Add Passenger</button>
-
-</div>
+      <button onClick={addPassenger}>Add Passenger</button>
+    </div>
   );
 }
 
