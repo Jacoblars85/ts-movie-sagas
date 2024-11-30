e4e9aef0daceee62f3822b9aa174b1ca77264854
Jacob Larson <hockeybobber@comcast.net>
2024-11-29 23:39:11 -0600

    added uselector for current movie

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { useSelector } from "react-redux";
+import { useHistory } from 'react-router-dom';
 
 function Details() {
+
+    const currentMovie = useSelector(store => store.currentMovie);
+
+    const goToHome = () => {
+
+        history.push(`/`)
+    }
+
 
   return (
     <div>
